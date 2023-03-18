5e109fde865141d2763969a991b71992d307d606
Kirnic21 <marcelokenji12121@gmail.com>
2023-03-18 10:18:55 -0300

    imported and exported functions

--- src/model.js
+++ src/model.js
@@ -1,17 +1,85 @@
-const toDoFactory = (title,description,dueDate,priority)=>{
-    return{title, description, dueDate,priority}
-}
-const projectFactory = (title,description,toDoarray)=>
-{
-    return{title,description,toDoarray}
-}
-const project = projectFactory("Start","Cool Project",[]);
-console.log(project)
-const todo= projectFactory("Do homework","Learn","12/32","High")
-project.toDoarray.push(todo)
+export {Todo,Project}
+class Todo {
+  constructor(title, description, dueDate, priority,status) {
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+    this.status = status
+  }
 
-const x = [1, 2, 3, 4, 5];
-const [y, z, m] = x;
-console.log(y); // 1
-console.log(z); // 2
-console.log(m);
+  get title() {
+    return this._title;
+  }
+  set title(title) {
+    this._title = title;
+  }
+
+  get description() {
+    return this._description;
+  }
+  set description(description) {
+    this._description = description;
+  }
+
+  get dueDate() {
+    return this._dueDate;
+  }
+  set dueDate(dueDate) {
+    this._dueDate = dueDate;
+  }
+
+  get priority() {
+    return this._priority;
+  }
+  set priority(priority) {
+    this._priority = priority;
+  }
+
+  get status() {
+    return this._status;
+  }
+  set status(status) {
+    this._status = status;
+  }
+  
+}
+
+class Project {
+  constructor(title, description) {
+    this.title = title;
+    this.description = description;
+    this.todos = [];
+  }
+
+  get title() {
+    return this._title;
+  }
+  set title(title) {
+    this._title = title;
+  }
+
+  get description() {
+    return this._description;
+  }
+  set description(description) {
+    this._description = description;
+  }
+
+  addTodo(todo) {
+    this.todos.push(todo);
+
+  }
+  removeTodo(index){
+   delete this.todos[index]
+  }
+  
+}
+
+let todo2 = new Todo("AAA", "Poop ", "2", "High");
+
+let project1 = new Project("POO","LOLOLOLOLOL");
+project1.addTodo(todo2);
+
+
+
